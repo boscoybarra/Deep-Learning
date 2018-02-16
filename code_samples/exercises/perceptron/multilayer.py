a3a96e3085e60efc3b4599b31222bb2a265bf8cc
boscoybarra <bosco@ybarra.io>
2018-02-16 23:52:43 +0100

    creating deeper neural networks

---
+++ code_samples/exercises/perceptron/multilayer.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1/(1+np.exp(-x))
+
+# Network size
+N_input = 4
+N_hidden = 3
+N_output = 2
+
+np.random.seed(42)
+# Make some fake data
+X = np.random.randn(4)
+
+weights_input_to_hidden = np.random.normal(0, scale=0.1, size=(N_input, N_hidden))
+weights_hidden_to_output = np.random.normal(0, scale=0.1, size=(N_hidden, N_output))
+
+# Make a forward pass through the network
+# We transpode the 1D array
+X = np.array([X])
+
+hidden_layer_in = np.dot(X, weights_input_to_hidden)
+print(hidden_layer_in.shape)
+print(weights_hidden_to_output.shape)
+
+hidden_layer_out = sigmoid(hidden_layer_in)
+
+print('Hidden-layer Output:')
+print(hidden_layer_out)
+
+output_layer_in = np.dot(hidden_layer_out, weights_hidden_to_output)
+# Creating a matrix for the output
+Y = np.array([N_output])
+Y = Y[:,None]
+
+output_layer_out = sigmoid(output_layer_in)
+
+print('Output-layer Output:')
+print(output_layer_out)
